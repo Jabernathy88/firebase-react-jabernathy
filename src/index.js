fdc281a9dbfece3db6c9750f639918378802aa8b
Jabernathy88 <j.e.abernathy@gmail.com>
2018-06-20 08:16:35 -0400

    Added Firebase connection and tested real-time database update.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import * as firebase from 'firebase';
 
+const config = {
+  apiKey: "xyz",
+  authDomain: "fir-react-jabernathy.firebaseapp.com",
+  databaseURL: "https://fir-react-jabernathy.firebaseio.com",
+  projectId: "fir-react-jabernathy",
+  storageBucket: "",
+};
+
 firebase.initializeApp(config)
 
 ReactDOM.render(<App />, document.getElementById('root'));
